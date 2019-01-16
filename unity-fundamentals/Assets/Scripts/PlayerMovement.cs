0df6f71c106a4e2c04d561cb48f6fbe356e7ade8
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-16 08:42:05 -0500

    moved left/right rotation to movement func

--- unity-fundamentals/Assets/Scripts/PlayerMovement.cs
+++ unity-fundamentals/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    public float speed = 5.0f;
+    public float moveSpeed = 5.0f;
     public float rotateSpeed = 60.0f;
 
 
@@ -13,7 +13,12 @@ public class PlayerMovement : MonoBehaviour {
 
         //Movement();
 
-        if(Input.GetKey (KeyCode.LeftArrow))
+    }
+
+    void Movement ()
+    {
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Key LeftArrow Pressed: Rotate");
 
@@ -25,47 +30,44 @@ public class PlayerMovement : MonoBehaviour {
 
             this.transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
         }
-    }
 
-    void Movement ()
-    {
         if (Input.GetKey(KeyCode.UpArrow))
         {
             Debug.Log("Key UpArrow Pressed.");
 
-            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            this.transform.Translate(new Vector3(0, 0, moveSpeed * Time.deltaTime));
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("Key DownArrow Pressed.");
 
-            this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+            this.transform.Translate(new Vector3(0, 0, -moveSpeed * Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Key LeftArrow Pressed.");
 
-            this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            this.transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Key RightArrow Pressed.");
 
-            this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            this.transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetKey(KeyCode.W))
         {
             Debug.Log("Key W Pressed.");
 
-            this.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            this.transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
         }
         else if (Input.GetKey(KeyCode.S))
         {
             Debug.Log("Key S Pressed.");
 
-            this.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            this.transform.Translate(new Vector3(0, -moveSpeed * Time.deltaTime, 0));
         }
     }
 }
