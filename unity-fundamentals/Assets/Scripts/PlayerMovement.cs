784c321237267f8179cdf9ca2a32bff56dc7baea
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-15 23:37:58 -0500

    added left rotate statement

--- unity-fundamentals/Assets/Scripts/PlayerMovement.cs
+++ unity-fundamentals/Assets/Scripts/PlayerMovement.cs
@@ -5,33 +5,47 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float speed = 5.0f;
+    public float rotateSpeed = 20.0f;
+
 
 	// Update is called once per frame
 	void Update () {
-        if( Input.GetKey(KeyCode.UpArrow) )
+        Movement();
+
+        if(Input.GetKey (KeyCode.LeftArrow))
+        {
+            Debug.Log("Key LeftArrow Pressed: Rotate");
+
+            this.transform.Rotate(new Vector3(0, rotateSpeed * Time.deltaTime, 0));
+        }
+    }
+
+    void Movement ()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
         {
             Debug.Log("Key UpArrow Pressed.");
 
-            this.transform.Translate( new Vector3(0, 0, speed * Time.deltaTime) );
-        } 
+            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             Debug.Log("Key DownArrow Pressed.");
 
-            this.transform.Translate( new Vector3(0, 0, -speed * Time.deltaTime ) );
+            this.transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
         }
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("Key LeftArrow Pressed.");
 
-            this.transform.Translate(new Vector3( -speed * Time.deltaTime, 0, 0 ));
+            this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
             Debug.Log("Key RightArrow Pressed.");
 
-            this.transform.Translate(new Vector3( speed * Time.deltaTime, 0, 0 ));
+            this.transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
 
         if (Input.GetKey(KeyCode.W))
