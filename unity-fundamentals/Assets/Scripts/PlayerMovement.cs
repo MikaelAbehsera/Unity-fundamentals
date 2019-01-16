e68ad1ae3d1d211124ec3b3fbd05bf0da0bc0f7f
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-15 21:20:59 -0500

    added moving logicv

--- unity-fundamentals/Assets/Scripts/PlayerMovement.cs
+++ unity-fundamentals/Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    public float speed = 5.0f;
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if( Input.GetKey(KeyCode.UpArrow) )
+        {
+            Debug.Log("Key UpArrow Pressed.");
+
+            this.transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+        }
+    }
 }
