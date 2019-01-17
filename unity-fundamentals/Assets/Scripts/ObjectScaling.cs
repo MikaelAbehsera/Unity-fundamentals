bafe18116ef87f65b809d7e969aa69521db46e7d
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-16 19:31:44 -0500

    added mega cube code

--- unity-fundamentals/Assets/Scripts/ObjectScaling.cs
+++ unity-fundamentals/Assets/Scripts/ObjectScaling.cs
@@ -4,13 +4,21 @@ using UnityEngine;
 
 public class ObjectScaling : MonoBehaviour {
 
+    public float scaleAmount = 3.0f;
+
 	// Use this for initialization
 	void Start () {
 		
 	}
 	
+    
+
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (Input.GetKeyDown(KeyCode.G))
+        {
+            this.transform.localScale = new Vector3(scaleAmount, scaleAmount, scaleAmount);
+            //this.transform.localScale = Vector3.one * scaleAmount;
+        }
+    }
 }
