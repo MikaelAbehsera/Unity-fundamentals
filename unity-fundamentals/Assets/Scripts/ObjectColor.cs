917121c05376f1513a8cf21d7d7e4be386ebf1a3
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-16 21:09:41 -0500

    changed og color class to private

--- unity-fundamentals/Assets/Scripts/ObjectColor.cs
+++ unity-fundamentals/Assets/Scripts/ObjectColor.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ObjectColor : MonoBehaviour {
 
     public Color changedColor = Color.white;
-    public Color originalColor = Color.white;
+    private Color originalColor = Color.white;
 
 
     /// <summary>
