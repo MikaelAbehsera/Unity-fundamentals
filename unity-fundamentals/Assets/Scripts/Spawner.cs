5e8840da46e2c2ac93ba75a54c1e1e1757167eb9
Mikael Abehsera <mikael.abehsera@gmail.com>
2019-01-17 23:21:01 -0500

    added forward force for spawn point

--- unity-fundamentals/Assets/Scripts/Spawner.cs
+++ unity-fundamentals/Assets/Scripts/Spawner.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class Spawner : MonoBehaviour {
 
     public GameObject spawnType = null;
-    public float scaleMin = 1.0f;
-    public float scaleMax = 10.0f;
+    public float scaleMin = 0.2f;
+    public float scaleMax = 1.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -22,12 +22,17 @@ public class Spawner : MonoBehaviour {
 
             obj.transform.position = this.transform.position;
 
+            float scaleSize = Random.Range(scaleMin, scaleMax);
+
+            obj.transform.localScale = Vector3.one * scaleSize;
+
             obj.AddComponent<Rigidbody>();
 
-            float scaleSize = Random.Range(scaleMin, scaleMax);
-            this.transform.localScale = Vector3.one * scaleSize;
+            obj.GetComponent<Rigidbody>().AddRelativeForce(Vector3.back * 20);
 
             obj.name = "mySphere";
+
+            // lecture 65 // no forward force // 2:13/4 
 
         }
 	}
